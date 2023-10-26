fa6ac01e4fcc505a514ba2248255a7cfcd7da647
LumoFace <lumoface@proton.me>
2023-10-26 05:41:46 +0000

    iscsi-target: Avoid early conn_logout_comp for iser connections

--- Plex/Regexps.py
+++ Plex/Regexps.py
@@ -176,4 +176,53 @@ class RE:
 #	 Primitive RE constructors
 #	 -------------------------
 #
-#	
+#	 These are the basic REs from which all others are built.
+#
+
+## class Char(RE):
+##	 """
+##	 Char(c) is an RE which matches the character |c|.
+##	 """
+	
+##	 nullable = 0
+	
+##	 def __init__(self, char):
+##		 self.char = char
+##		 self.match_nl = char == '\n'
+		
+##	 def build_machine(self, m, initial_state, final_state, match_bol, nocase):
+##		 c = self.char
+##		 if match_bol and c <> BOL:
+##			 s1 = self.build_opt(m, initial_state, BOL)
+##		 else:
+##			 s1 = initial_state
+##		 if c == '\n' or c == EOF:
+##			 s1 = self.build_opt(m, s1, EOL)
+##		 if len(c) == 1:
+##			 code = ord(self.char)
+##			 s1.add_transition((code, code+1), final_state)
+##			 if nocase and is_letter_code(code):
+##				 code2 = other_case_code(code)
+##				 s1.add_transition((code2, code2+1), final_state)
+##		 else:
+##			 s1.add_transition(c, final_state)
+
+##	 def calc_str(self):
+##		 return "Char(%s)" % repr(self.char)
+
+def Char(c):
+	"""
+	Char(c) is an RE which matches the character |c|.
+	"""
+	if len(c) == 1:
+		result = CodeRange(ord(c), ord(c) + 1)
+	else:
+		result = SpecialSymbol(c)
+	result.str = "Char(%s)" % repr(c)
+	return result
+
+class RawCodeRange(RE):
+	"""
+	RawCodeRange(code1, code2) is a low-level RE which matches any character
+	with a code |c| in the range |code1| <= |c| < |code2|, where the range
+	does not include newline. For internal use only
