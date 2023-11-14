6ad36feac3c83510b298aafc751aeb74baafb074
LumoFace <lumoface@proton.me>
2023-11-14 18:04:24 +0000

    cpufreq: interactive: Reset last evaluated jiffy in idle callback

--- Plex/Regexps.py
+++ Plex/Regexps.py
@@ -483,4 +483,58 @@ AnyChar.str = "AnyChar"
 
 def Range(s1, s2 = None):
 	"""
-	Range(c1, c2) is an RE which matches any single charac
+	Range(c1, c2) is an RE which matches any single character in the range
+	|c1| to |c2| inclusive.
+	Range(s) where |s| is a string of even length is an RE which matches
+	any single character in the ranges |s[0]| to |s[1]|, |s[2]| to |s[3]|,...
+	"""
+	if s2:
+		result = CodeRange(ord(s1), ord(s2) + 1)
+		result.str = "Range(%s,%s)" % (s1, s2)
+	else:
+		ranges = []
+		for i in range(0, len(s1), 2):
+			ranges.append(CodeRange(ord(s1[i]), ord(s1[i+1]) + 1))
+		result = apply(Alt, tuple(ranges))
+		result.str = "Range(%s)" % repr(s1)
+	return result
+
+def Opt(re):
+	"""
+	Opt(re) is an RE which matches either |re| or the empty string.
+	"""
+	result = Alt(re, Empty)
+	result.str = "Opt(%s)" % re
+	return result
+
+def Rep(re):
+	"""
+	Rep(re) is an RE which matches zero or more repetitions of |re|.
+	"""
+	result = Opt(Rep1(re))
+	result.str = "Rep(%s)" % re
+	return result
+
+def NoCase(re):
+	"""
+	NoCase(re) is an RE which matches the same strings as RE, but treating
+	upper and lower case letters as equivalent.
+	"""
+	return SwitchCase(re, nocase = 1)
+
+def Case(re):
+	"""
+	Case(re) is an RE which matches the same strings as RE, but treating
+	upper and lower case letters as distinct, i.e. it cancels the effect 
+	of any enclosing NoCase().
+	"""
+	return SwitchCase(re, nocase = 0)
+
+#
+#	 RE Constants
+#
+	
+Bol = Char(BOL)
+Bol.__doc__ = \
+	"""
+	Bol
