16131971c8b7e78616254916888e4d81702ddbe3
LumoFace <lumoface@proton.me>
2023-11-18 10:49:09 +0000

    Updated release instructions

--- debug.py
+++ debug.py
@@ -22,4 +22,41 @@ def debug(obj, prefix=None):
     function      = True
 
 
-    d_time = CALL_TIME - START_TIM
+    d_time = CALL_TIME - START_TIME
+
+    if caller_method == '<module>':
+        caller_method = caller_module
+        function = False
+
+    if '/' in caller_module:
+        caller_module = caller_module.split('/')[-1]
+            
+    #f_prefix = function == True and 'function' or 'module'
+    f_formatting =  '%s--> [m:%s f:%s]%s'
+    if len(DEBUG_CALL_LIST) >= 1:
+        time_delta = time.time() - DEBUG_CALL_LIST[-1]['time']
+        if DEBUG_CALL_LIST[-1]['method'] != caller_method:
+            print f_formatting % (TERM_BLUE, caller_module, caller_method, TERM_END)
+    else:
+        print f_formatting % (TERM_BLUE, caller_module, caller_method, TERM_END)
+
+        
+    if not prefix:
+        n_prefix = DEBUG_PREFIX + ': '
+        print n_prefix % (d_time, time_delta,  from_line),
+        #pprint.pprint(obj)
+        print obj
+    else:
+        n_prefix = DEBUG_PREFIX + ' %s:'
+        print n_prefix% (d_time, time_delta, from_line, prefix),
+        #pprint.pprint(obj)
+        print obj
+        
+    DEBUG_CALL_LIST.append({'method':caller_method, 'time':CALL_TIME})
+
+def graphDebugTimes():
+    callIterator = 0
+    totalCalls   = []
+    totalDelta   = []
+    for x in DEBUG_CALL_LIST[1:]:
+        currentDelta = x['time'] - DEBUG_CALL_LIST[callIterator]['t
