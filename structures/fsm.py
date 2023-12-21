1bcc13d4b7f889a47fd13831a24ff0de72be08d0
LumoFace <lumoface@proton.me>
2023-12-21 06:12:42 +0000

    Renommage

--- structures/fsm.py
+++ structures/fsm.py
@@ -5,4 +5,30 @@ this FSM also maintains a user defined "memory". So this FSM can be used as a
 Push-down Automata (PDA) since a PDA is a FSM + memory.
 
 The following describes how the FSM works, but you will probably also need to
-see the exam
+see the example function to understand how the FSM is used in practice.
+
+You define an FSM by building tables of transitions. For a given input symbol
+the process() method uses these tables to decide what action to call and what
+the next state will be. The FSM has a table of transitions that associate:
+
+        (input_symbol, current_state) --> (action, next_state)
+
+Where "action" is a function you define. The symbols and states can be any
+objects. You use the add_transition() and add_transition_list() methods to add
+to the transition table. The FSM also has a table of transitions that
+associate:
+
+        (current_state) --> (action, next_state)
+
+You use the add_transition_any() method to add to this transition table. The
+FSM also has one default transition that is not associated with any specific
+input_symbol or state. You use the set_default_transition() method to set the
+default transition.
+
+When an action function is called it is passed a reference to the FSM. The
+action function may then access attributes of the FSM such as input_symbol,
+current_state, or "memory". The "memory" attribute can be any object that you
+want to pass along to the action functions. It is not used by the FSM itself.
+For parsing you would typically pass a list to be used as a stack.
+
+The processing sequence is as follows. The proces
